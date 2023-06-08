5e5a16a54bf61d9ffbaa540cb046b426038ded88
dervish99 <uddinn9@gmail.com>
2023-06-08 13:31:00 +0300

    added username and password input on loginform

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,7 +3,68 @@ import { useState } from "react";
 import FirebaseAuthService from "../FirebaseAuthService";
 
 function LoginForm({ existingUser }) {
-  return <h1>Login Form</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    //prevent the default behavior of an event from occurring, to customise it yourself
+    event.preventDefault();
+
+    //try/catch best pratcie for handling js asyn requests and promises
+    try {
+      await FirebaseAuthService.registerUser(username, password);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function handleLogout() {
+    FirebaseAuthService.logoutUser();
+  }
+  return (
+    <div className="login-form-container">
+      {existingUser ? (
+        <div className="row">
+          <h3>Welcome, {existingUser.email}</h3>
+          <button
+            type="button"
+            className="primar-button"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="login-form">
+          <label className="input-label login-label">
+            Username (email):
+            <input
+              type="email"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <label className="input-label login-label">
+            Password (email):
+            <input
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <div className="button-box">
+            <button className="primary-button">Submit</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default LoginForm;
